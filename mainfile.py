9618b4ab8b6ee97473f35891df5d6fff3de1a8e0
Donald McClymont <Donaldm2020@gmail.com>
2021-12-28 19:13:08 +0000

    pep8

--- mainfile.py
+++ mainfile.py
@@ -6,19 +6,19 @@ import cv2
 
 img = cv2.imread("donald.jpg")
 
-## Image to Gray Image
+# Image to Gray Image
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-## Gray Image to Inverted Gray Image
+# Gray Image to Inverted Gray Image
 inverted_gray_image = 255 - gray_image
 
-## Blurring The Inverted Gray Image
+# Blurring The Inverted Gray Image
 blurred_inverted_gray_image = cv2.GaussianBlur(inverted_gray_image, (19, 19), 0)
 
-## Inverting the blurred image
+# Inverting the blurred image
 inverted_blurred_image = 255 - blurred_inverted_gray_image
 
-### Preparing Photo sketching
+# Preparing Photo sketching
 sketck = cv2.divide(gray_image, inverted_blurred_image, scale=256.0)
 
 cv2.imshow("Original Image", img)
@@ -30,4 +30,3 @@ filename = 'sketch.jpg'
 cv2.imwrite(filename, sketck)
 
 cv2.waitKey(0)
-
